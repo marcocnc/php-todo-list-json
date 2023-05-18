da0f647c4c31427b209ed3938c5bd145ee18fc1a
marcocnc <marcocancelliere56@gmail.com>
2023-05-18 14:53:36 +0200

    created arr in file json and printed in php server

---
+++ server.php
@@ -0,0 +1,14 @@
+<?php 
+
+//  Prendo i contenuti del file json
+$json_string = file_get_contents('task-list.json');
+
+// Li leggo
+$array_tasks = json_decode($json_string);
+
+// Trasformo in un file json
+header('Content-Type: application/json');
+
+// Lo stampo 
+echo json_encode($array_tasks);
+?>
