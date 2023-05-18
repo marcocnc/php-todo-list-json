8c1dcaf226ab518031b0bd526d5206a5d56526a3
marcocnc <marcocancelliere56@gmail.com>
2023-05-18 14:35:55 +0200

    created server.php and draft of API function in js

--- main.js
+++ main.js
@@ -4,8 +4,18 @@ createApp({
 
     data(){
         return{
-
+            apiUrl: 'server.php'  // Endpoint
         }
+    },
+
+    methods:{
+        getApi(){
+            console.log('Questa sarà la chiamata API');
+        }
+    },
+
+    mounted(){
+        this.getApi();
     }
     
 }).mount('#app');
